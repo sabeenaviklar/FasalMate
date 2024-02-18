f914589cb2477fb7893ec2394a1f94b40cd7cfd6
Sabeena Viklar <viklarsabeena@gmail.com>
2024-02-18 18:53:13 +0530

    app added

---
+++ app/page.tsx
@@ -0,0 +1,17 @@
+import Hero from "@/components/Hero";
+import Farm from "@/components/Farm";
+import Guide from "@/components/Guide";
+import Features from "@/components/Features";
+import GetApp from "@/components/GetApp";
+
+export default function Home() {
+  return (
+ <>
+     <Hero/>
+    {/* <Farm/> */}
+     <Guide/>
+     <Features/>
+     <GetApp/>
+ </>
+  );
+}
